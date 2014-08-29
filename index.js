9828e6cc40950a4439d814cf0568c9234685460e
Kevin Woo <kevinawoo@gmail.com>
2014-08-29 14:57:28 -0500

    reformat code

--- index.js
+++ index.js
@@ -1,81 +1,81 @@
 var mongoose = require('mongoose'),
-    owl = require('owl-deepcopy');
+	owl = require('owl-deepcopy');
 
 var Schema = mongoose.Schema,
-    Model = mongoose.Model;
+	Model = mongoose.Model;
 
 /**
  * Add a new function to the schema prototype to create a new schema by extending an existing one
  */
-Schema.prototype.extend = function(obj, options) {
-  
-  // Deep clone the existing schema so we can add without changing it
-  var newSchema = owl.deepCopy(this);
+Schema.prototype.extend = function (obj, options) {
 
-  // Fix for callQueue arguments, todo: fix clone implementation
-  newSchema.callQueue.forEach(function(k) {
-    var args = [];
-    for(var i in k[1]) {
-      args.push(k[1][i]);
-    }
-    k[1] = args;
-  });
+	// Deep clone the existing schema so we can add without changing it
+	var newSchema = owl.deepCopy(this);
 
-  // Override the existing options with any newly supplied ones
-  for(var k in options) {
-    newSchema.options[k] = options[k];
-  }
+	// Fix for callQueue arguments, todo: fix clone implementation
+	newSchema.callQueue.forEach(function (k) {
+		var args = [];
+		for (var i in k[1]) {
+			args.push(k[1][i]);
+		}
+		k[1] = args;
+	});
 
-  // This adds all the new schema fields
-  newSchema.add(obj);
+	// Override the existing options with any newly supplied ones
+	for (var k in options) {
+		newSchema.options[k] = options[k];
+	}
 
-  var key = newSchema.options.discriminatorKey;
-  if(key) {
-    // If a discriminatorField is in the schema options, add a new field to store model names
-    var discriminatorField = {};
-    discriminatorField[key] = { type : String };
-    newSchema.add(discriminatorField);
+	// This adds all the new schema fields
+	newSchema.add(obj);
 
-    // When new documents are saved, include the model name in the discriminatorField
-    // if it is not set already.
-    newSchema.pre('save', function(next) {
-      if(this[key] === null || this[key] === undefined) {
-        this[key] = this.constructor.modelName;
-      }
-      next();
-    });
-  }
+	var key = newSchema.options.discriminatorKey;
+	if (key) {
+		// If a discriminatorField is in the schema options, add a new field to store model names
+		var discriminatorField = {};
+		discriminatorField[key] = { type: String };
+		newSchema.add(discriminatorField);
 
-  return newSchema;
+		// When new documents are saved, include the model name in the discriminatorField
+		// if it is not set already.
+		newSchema.pre('save', function (next) {
+			if (this[key] === null || this[key] === undefined) {
+				this[key] = this.constructor.modelName;
+			}
+			next();
+		});
+	}
+
+	return newSchema;
 };
 
 /**
  * Wrap the model init to set the prototype based on the discriminator field
  */
 var oldInit = Model.prototype.init;
-Model.prototype.init = function(doc, query, fn) {
-  var key = this.schema.options['discriminatorKey'];
-  if(key) {
+Model.prototype.init = function (doc, query, fn) {
+	var key = this.schema.options['discriminatorKey'];
+	if (key) {
 
-    // If the discriminatorField contains a model name, we set the documents prototype to that model
-    var type = doc[key];
-      // this will throw exception if the model isn't registered
-    if(type) {
-      var model = (this.db.models? this.db.models[type] : mongoose.models[type]);
-      var newFn = function() {
-        // this is pretty ugly, but we need to run the code below before the callback
-        process.nextTick(function() {
-          if(fn) fn.apply(this, arguments);
-        });
-      }
-      var modelInstance = new model();
-      this.schema = model.schema;
-      var obj = oldInit.call(this, doc, query, newFn);
-      obj.__proto__ = model.prototype;
-      return obj;
-    }
-  }
+		// If the discriminatorField contains a model name, we set the documents prototype to that model
+		var type = doc[key];
+		// this will throw exception if the model isn't registered
+		if (type) {
+			var model = (this.db.models ? this.db.models[type] : mongoose.models[type]);
+			var newFn = function () {
+				// this is pretty ugly, but we need to run the code below before the callback
+				process.nextTick(function () {
+					if (fn) fn.apply(this, arguments);
+				});
+			}
+			var modelInstance = new model();
+			this.schema = model.schema;
+			var obj = oldInit.call(this, doc, query, newFn);
+			obj.__proto__ = model.prototype;
+			return obj;
+		}
+	}
 
-  // If theres no discriminatorKey we can just call the original method
-  return oldInit.apply(this, arguments);
+	// If theres no discriminatorKey we can just call the original method
+	return oldInit.apply(this, arguments);
 }
